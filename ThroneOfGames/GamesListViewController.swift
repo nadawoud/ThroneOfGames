5c098b0f665aba1afb5cf5fc00fd83ec59ade3de
Nada Yehia Dawoud <nadawoud@gmail.com>
2021-04-27 15:08:28 +0200

    Rename ViewController GamesListViewController and change it to TableViewController

---
+++ ThroneOfGames/GamesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThroneOfGames
+//
+//  Created by Nada Yehia Dawoud on 22/04/2021.
+//
+
+import UIKit
+
+class GamesListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
